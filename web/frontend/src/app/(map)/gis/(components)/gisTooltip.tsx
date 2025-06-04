7f039cf0b13a42260eda2d144b7d06b97ffe4bc5
shellcodesniper <root@ql.gl>
2025-06-04 18:20:25 +0900

    feat<web>: comp

--- web/frontend/src/app/(map)/gis/(components)/gisTooltip.tsx
+++ web/frontend/src/app/(map)/gis/(components)/gisTooltip.tsx
@@ -18,7 +18,7 @@ export function gisToolTip({
     <div
       className={cn(
         'y-[0px] relative z-50 m-0 h-64 w-72 overflow-hidden',
-        "rounded-sm bg-black/70 p-0 font-['Pretendard'] text-[6px] text-white",
+        "rounded-sm bg-black/70 p-0 font-['Pretendard'] text-[12px] text-white",
       )}
     >
       {/* 상단 타이틀 */}
@@ -27,7 +27,7 @@ export function gisToolTip({
           <svg width='6' height='6' viewBox='0 0 6 6' fill='none'>
             <circle cx='3' cy='3' r='2.5' fill='#FFE345' />
           </svg>
-          <span className='text-[7.5px] font-semibold leading-[10px]'>
+          <span className='text-[15px] font-semibold leading-[10px]'>
             {sidonm} {sggnm}
           </span>
         </div>
@@ -38,7 +38,7 @@ export function gisToolTip({
               fill='#969696'
             />
           </svg>
-          <span className='text-[10px]'>1/1</span>
+          <span className='text-[16px]'>1/1</span>
           <svg width='20' height='20' viewBox='0 0 10 10' fill='none'>
             <path
               d='M6.44 4.66L4.7 2.91L4.97 2.65L6.98 4.66L4.97 6.67L4.7 6.4L6.44 4.66Z'
@@ -49,10 +49,10 @@ export function gisToolTip({
       </div>
 
       {/* 구분선 */}
-      <div className='absolute left-[12px] top-[48px] h-[2px] w-[137px] bg-gray-400 opacity-50'></div>
+      <div className='absolute left-[12px] top-[48px] h-[2px] w-[272] bg-gray-400 opacity-50'></div>
 
       {/* 내용 */}
-      <div className='absolute left-[8px] top-[28px] flex w-[130px] flex-col gap-1'>
+      <div className='absolute left-[8px] top-[28px] flex w-[260] flex-col gap-1'>
         <div className='flex justify-between'>
           <span>논 / 밭</span>
           <span>{farmmap_count}구</span>
@@ -69,7 +69,7 @@ export function gisToolTip({
           </span>
         </div>
         <div className='flex justify-between'>
-          <span className='w-16'>데이터 기준일</span>
+          <span className='w-32'>데이터 기준일</span>
           <span></span>
         </div>
         <div className='flex justify-between'>
@@ -80,10 +80,10 @@ export function gisToolTip({
 
       {/* 버튼 영역 */}
       <div className='absolute bottom-2 left-2 flex gap-2'>
-        <button className='w-16 rounded bg-black/60 px-2 py-1 text-[6.5px] font-medium text-white'>
+        <button className='w-32 rounded bg-black/60 px-2 py-1 text-[13px] font-medium text-white'>
           기록열람
         </button>
-        <button className='w-16 rounded bg-black/60 px-2 py-1 text-[6.5px] font-medium text-white'>
+        <button className='w-32 rounded bg-black/60 px-2 py-1 text-[13px] font-medium text-white'>
           장애 이력 보기
         </button>
       </div>
